3f8a00a8f84d2d0d3125ee1204c17346b01420e2
Chronique <adhi.chronique@gmail.com>
2026-03-18 05:45:29 +0700

    fix next

--- karambit-app/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  transpilePackages: ["@stacks/transactions", "@stacks/network"],
-  serverExternalPackages: ["@stacks/connect"],
-};
-
-export default nextConfig;
